a59a663ee2e5b137368a57fbb3664f68bbcb054f
XuChao <xutongle@gmail.com>
2017-08-21 10:31:15 +0800

    init

--- TagsinputAsset.php
+++ TagsinputAsset.php
@@ -4,17 +4,18 @@
  * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
  * @license http://www.tintsoft.com/license/
  */
+
 namespace xutl\bootstrap\tagsinput;
 
 use yii\web\AssetBundle;
 
 /**
  * Class TagsinputAsset
- * @package yuncms\tag
+ * @package xutl\bootstrap\tagsinput
  */
 class TagsinputAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/bootstrap-tagsinput';
+    public $sourcePath = '@bower/bootstrap-tagsinput/dist';
 
     public $css = [
         'bootstrap-tagsinput.css',
